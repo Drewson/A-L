e9261c902ef7f422467b486f1ea30fd5ec748ca3
andrew.nelsonrd@gmail.com <andrew.nelsonrd@gmail.com>
2017-05-08 11:20:51 -0700

    footer implementation bug, removed for now

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <MuiThemeProvider>
         <MainHeader />
-        <MainFooter />
+        
       </MuiThemeProvider>
     );
   }
--- src/partials/footer.js
+++ src/partials/footer.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import AppBar from 'material-ui/AppBar';
-import FontAwesome from 'react-fontawesome';
 
 import '../App.css';
 
-const MainFooter = () => {
+class MainFooter extends Component {
+  render() {
     return (
     <footer style={{height: '300px', width:'100%', backgroundColor:'black'}}>
     </footer>
     );
+  }
 }
 
 export default MainFooter;
