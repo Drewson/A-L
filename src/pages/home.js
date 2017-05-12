0d901a2d54ce514b8048501fb23ff72e611362bb
andrew.nelsonrd@gmail.com <andrew.nelsonrd@gmail.com>
2017-05-12 10:58:11 -0700

    added a test text block to front page

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,8 +4,8 @@ import './style.css';
 class Home extends Component {
     render(){
         return(
-            <div>
-                <div className='featuredListing'>
+            <div className='home'>
+                <section className='featuredListing'>
                     <span className='textWrapper'>
                         <div className='featuredHeader'>
                             <h2>Featured Listing</h2>
@@ -14,16 +14,21 @@ class Home extends Component {
                         by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.
                         </p>
                     </span>
-                </div>
-                <div className='founders'>
-                    <h2>A - L</h2>
+                </section>
+                <h2 className='founderHead'>A - L</h2>
+                <section className='founders'>
                     <div className='alex'>
                         <h3>Alex</h3>
                     </div>
                     <div className='leon'>
                         <h3>Leon</h3>
                     </div>
-                </div>
+                </section>
+                <section className='frontText'>
+                    <p>
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                    </p>
+                </section>
             </div>
         )
     }
