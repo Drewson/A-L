d05c4793320c0d6bf69e27ed3eee1af07103ddd3
andrew.nelsonrd@gmail.com <andrew.nelsonrd@gmail.com>
2017-05-15 18:09:07 -0700

    fixed margintop

--- src/App/App.js
+++ src/App/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div>
         <MainHeader />
-          <div style={{marginTop:'185px'}}>
+          <div style={{marginTop:'180px'}}>
             { this.props.children }
           </div>
         <MainFooter />
